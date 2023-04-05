c82feb9dbb1e4a7de47d068504f3539a2215aa90
Sara Withrow <saralouise1107@gmail.com>
2023-04-05 17:37:34 -0400

    new

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers){ 
+    return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    return (someValue) => {return someValue * multiplier};
+}
+
+function fareDoubler(someValue){
+    const multiplier = createFareMultiplier(2);
+    return multiplier(someValue);
+
+}
+
+function fareTripler(someValue){
+    const multiplier = createFareMultiplier(3);
+    return multiplier(someValue);
+}
+
+function selectDifferentDrivers(arrayOfDrivers, select){
+    return select(arrayOfDrivers);
+}
